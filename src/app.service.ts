0b530fbefd4c6019b953d3d1a84ee85d6ba24036
MateuszMyszke <myszke93@gmail.com>
2023-07-27 15:09:12 +0200

    Add first endpoint

---
+++ src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    const name = 'John'
+    const text = `Hello ${name}`;
+    return text;
+  }
+}
