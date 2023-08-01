769e3b7adb9985381f87036878e753f4d451bf6f
MateuszMyszke <myszke93@gmail.com>
2023-08-01 18:06:13 +0200

    Add error capture

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Delete, Post, Body, ParseUUIDPipe, Put } from '@nestjs/common';
+import { Controller, Get, Param, Delete, Post, Body, ParseUUIDPipe, Put, NotFoundException } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDTO } from './dtos/create-product.dto';
 import { UpdateProductDTO } from './dtos/update-product.dto';
@@ -13,11 +13,15 @@ export class ProductsController {
 
   @Get('/:id')
   public getById(@Param('id', new ParseUUIDPipe()) id: string) {
-    return this.productsService.getById(id);
+    const prod = this.productsService.getById(id);
+    if(!prod) throw new NotFoundException('Product not found');
+    return prod;
   }
 
   @Delete('/:id')
   deleteById(@Param('id', new ParseUUIDPipe()) id: string){
+    if(!this.productsService.getById(id))
+      throw new NotFoundException('Product not found');
     this.productsService.deleteByID(id);
     return { success: true};
   }
@@ -31,6 +35,9 @@ export class ProductsController {
   update(
     @Param('id', new ParseUUIDPipe()) id: string, 
     @Body() productData: UpdateProductDTO) {
+    if(!this.productsService.getById(id))
+      throw new NotFoundException('Product not found');
+
     this.productsService.updateById(id, productData);
     return { success: true };
   }
